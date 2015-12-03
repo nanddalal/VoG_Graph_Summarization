31debe58a8118dda54d21c851436b763433836fb
Nand Dalal <nndalal@umich.edu>
2015-12-03 17:32:03 -0500

    Passing the queue works, but top k is not being updated

--- STRUCTURE_DISCOVERY_PY/summarization.py
+++ STRUCTURE_DISCOVERY_PY/summarization.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import os
 import sys
 import signal
 import csv
@@ -23,12 +22,11 @@ class VoGTimeout(Exception):
         raise VoGTimeout
 
 
-# TODO: probably should move these into VoG
 top_k = 10
 top_k_structures = []
-top_k_structures_lock = mp.Lock()
 gcc_queue_lock = mp.Lock()
 gcc_queue_cv = mp.Condition(gcc_queue_lock)
+
 
 class VoG:
     def __init__(self, input_file, hubset_k=1, time_limit=None, parallel=True):
@@ -38,11 +36,10 @@ class VoG:
 
         self.parallel = parallel
         if parallel:
-            self.q = mp.Queue()
+            self.manager = mp.Manager()
+            self.top_k_queue = self.manager.Queue()
             self.workers = mp.Pool(processes=(mp.cpu_count() * 2))
-            self.workers.apply_async(update_topk)
-            # top_k_handler = mp.Process(target=update_topk, args=(self.q,))
-            # top_k_handler.start()
+            self.workers.apply_async(update_top_k)
 
         if time_limit is not None:
             signal.signal(signal.SIGALRM, VoGTimeout.time_limit_handler)
@@ -58,8 +55,7 @@ class VoG:
             signal.alarm(0)  # TODO: understand why this is necessary
 
         print "Printing top k structures"
-        self.print_top_k_structures()
-        top_k_handler.join()
+        VoG.print_top_k_structures()
         # self.visualize_graph()
         # plt.show()
         # plt.close()
@@ -71,7 +67,8 @@ class VoG:
         nx.draw_networkx_edges(self.G, pos)
         nx.draw_networkx_labels(self.G, pos)
 
-    def print_top_k_structures(self):
+    @staticmethod
+    def print_top_k_structures():
         top_k_structures.sort(reverse=True)
         for s in top_k_structures:
             print s[1].__class__.__name__, s[1].graph.nodes()
@@ -110,32 +107,23 @@ class VoG:
             k: number of hubsets to remove at each iteration
             gcc_num_nodes_criterion: the inclusive upper-bound criterion for a subgraph to be a GCC which will be burned
         """
-        debug_print("hI")
         sys.stdout.flush()
         self.gcc_queue = [self.G]
 
         i = 0
         while True:
             i += 1
-            debug_print("hI" + str(i))
-            debug_print("acquiring lock")
             gcc_queue_lock.acquire()
-            debug_print("acquired lock")
             while len(self.gcc_queue) <= 0:
                 gcc_queue_cv.wait()
-            debug_print("waited for the queue to have an elt")
             current_gcc = self.gcc_queue[0]
             del self.gcc_queue[0]
-            debug_print("did some work and unlocking")
             gcc_queue_lock.release()
-            debug_print("unlocked")
 
             if self.parallel:
-                debug_print("hIfd" + str(i))
                 self.workers.apply_async(slash_and_burn,
-                                         args=(current_gcc, hubset_k, gcc_num_nodes_criterion, self.total_num_nodes, self.q),
+                                         args=(current_gcc, hubset_k, gcc_num_nodes_criterion, self.total_num_nodes, self.top_k_queue),
                                          callback=self.collect_slashburned_gccs)
-                self.workers.apply_async(test_func, callback=self.lol)
             else:
                 self.collect_slashburned_gccs(
                     slash_and_burn(current_gcc, hubset_k, gcc_num_nodes_criterion, self.total_num_nodes))
@@ -144,33 +132,16 @@ class VoG:
             self.workers.close()
             self.workers.join()
 
-    def lol(self, r):
-        print r, "called back :)"
-
     def collect_slashburned_gccs(self, gccs):
-        # TODO: use CV's here to avoid busy waiting
         try:
-            debug_print("in callback acquiring lock")
             gcc_queue_lock.acquire()
-            debug_print("acquiring lock and about to do work")
             self.gcc_queue += gccs
-            debug_print("notifying that i have updated the gcc_queue")
             gcc_queue_cv.notify()
-            debug_print("notified that i have updated the gcc_queue")
         finally:
-            debug_print("in call back releasin the lock")
             gcc_queue_lock.release()
-            debug_print("in call back released the lock")
 
 
-def test_func():
-    print "yo"
-    return 1
-
-
-def slash_and_burn(current_gcc, hubset_k, gcc_num_nodes_criterion, total_num_nodes, q):
-    print "yoyoyoyoyo"
-    debug_print("YOYOYO?!?!")
+def slash_and_burn(current_gcc, hubset_k, gcc_num_nodes_criterion, total_num_nodes, top_k_queue):
     gccs = []
 
     print "Finding k hubset", current_gcc.number_of_nodes(), current_gcc.number_of_edges()
@@ -185,8 +156,7 @@ def slash_and_burn(current_gcc, hubset_k, gcc_num_nodes_criterion, total_num_nod
     for node in k_hubset:
         hubset_subgraph = current_gcc.neighbors(node)
         hubset_subgraph.append(node)
-        # update_top_k(mdl_encoding(current_gcc.subgraph(hubset_subgraph), total_num_nodes))
-        q.put(mdl_encoding(current_gcc.subgraph(hubset_subgraph), total_num_nodes))
+        top_k_queue.put(mdl_encoding(current_gcc.subgraph(hubset_subgraph), total_num_nodes))
 
     # remove the k hubset from G, so now we have G' (slash!)
     current_gcc.remove_nodes_from(k_hubset)
@@ -202,31 +172,28 @@ def slash_and_burn(current_gcc, hubset_k, gcc_num_nodes_criterion, total_num_nod
     # iterate over the remaining subgraphs we are "burning"
     for sub_graph in sub_graphs:
         if sub_graph.number_of_nodes() <= gcc_num_nodes_criterion:
-            q.put(mdl_encoding(sub_graph, total_num_nodes))
+            top_k_queue.put(mdl_encoding(sub_graph, total_num_nodes))
         else:
             # append the subgraph to GCCs queue
             gccs.append(sub_graph)
 
-    debug_print("RETURN CMON!!!")
     return gccs
 
 
-def update_top_k(structure):
-    # TODO: use CV's here to avoid busy waiting
-    try:
-        top_k_structures_lock.acquire()
-        if len(top_k_structures) < top_k:
-            print "Adding", structure.__class__.__name__
-            sys.stdout.flush()
-            heapq.heappush(top_k_structures, (structure.benefit, structure))
-        else:
-            if top_k_structures[0][0] < structure.benefit:
-                print "Adding", structure.__class__.__name__, \
-                    "and removing", top_k_structures[0][1].__class__.__name__
-                heapq.heappushpop(top_k_structures, (structure.benefit, structure))
-                sys.stdout.flush()
-    finally:
-        top_k_structures_lock.release()
+def update_top_k(top_k_queue):
+    while True:
+        structure = top_k_queue.get()
+        try:
+            if len(top_k_structures) < top_k:
+                print "Adding", structure.__class__.__name__
+                heapq.heappush(top_k_structures, (structure.benefit, structure))
+            else:
+                if top_k_structures[0][0] < structure.benefit:
+                    print "Adding", structure.__class__.__name__, \
+                        "and removing", top_k_structures[0][1].__class__.__name__
+                    heapq.heappushpop(top_k_structures, (structure.benefit, structure))
+        finally:
+            pass
 
 
 def mdl_encoding(sub_graph, total_num_nodes):
@@ -247,30 +214,10 @@ def mdl_encoding(sub_graph, total_num_nodes):
     return optimal_structure
 
 
-def update_topk(q):
-    while True:
-        structure = q.get()
-        try:
-            # top_k_structures_lock.acquire()
-            if len(top_k_structures) < top_k:
-                print "Adding", structure.__class__.__name__
-                heapq.heappush(top_k_structures, (structure.benefit, structure))
-                sys.stdout.flush()
-            else:
-                if top_k_structures[0][0] < structure.benefit:
-                    print "Adding", structure.__class__.__name__, \
-                        "and removing", top_k_structures[0][1].__class__.__name__
-                    heapq.heappushpop(top_k_structures, (structure.benefit, structure))
-                    sys.stdout.flush()
-        finally:
-            pass
-            # top_k_structures_lock.release()
-
-
-
 def debug_print(debug):
     print debug
     sys.stdout.flush()
+
 
 if __name__ == '__main__':
     vog = VoG('../DATA/soc-Epinions1.txt', time_limit=60, parallel=True)
